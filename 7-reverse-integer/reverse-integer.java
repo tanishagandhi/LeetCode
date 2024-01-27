9995a6102df1a6df07535668e026cf54f759a23b
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-01-27 21:24:55 +0530

    Time: 1 ms (92.63%) | Memory: 41.1 MB (14.83%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -18,4 +18,22 @@ class Solution {
         return rev;
     }
 }
+// python:
+// def reverse(x):
+//     INT_MAX = 2**31 - 1
+//     INT_MIN = -2**31
+
+//     rev = 0
+//     while x != 0:
+//         d = x % 10
+//         new_rev = rev * 10 + d
+
+//         # Check for integer overflow before updating rev
+//         if new_rev > INT_MAX or new_rev < INT_MIN:
+//             return 0  # Overflow/Underflow
+
+//         rev = new_rev
+//         x //= 10
+
+//     return rev
 
