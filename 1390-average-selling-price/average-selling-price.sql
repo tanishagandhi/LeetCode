3e599f8ba632c151af09bd9b455b28e71044e6ae
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-02-23 16:35:24 +0530

    Time: 1247 ms (87.39%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select p.product_id,coalesce(ROUND((sum(p.price*u.units)/sum(u.units)),2),0) as average_price 
+from Prices as p
+left join UnitsSold as u 
+on p.product_id=u.product_id && u.purchase_date BETWEEN p.start_date AND
+p.end_date
+group by p.product_id;
