e702d4c35d98ecba108ce263b364e79cc37cb5db
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-03-11 22:36:49 +0530

    Time: 1924 ms (75.58%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,1 +1,1 @@
-select p.product_name,s.year,s.price from Sales as s left join Product as p on s.product_id=p.product_id;
+select p.product_name,s.year,s.price from Sales as s inner join Product as p on s.product_id=p.product_id;
