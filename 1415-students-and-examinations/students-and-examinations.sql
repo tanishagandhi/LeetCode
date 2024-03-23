74400f5b788b7ce8bed22aa3073842077a50432c
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-03-23 13:57:31 +0530

    Time: 1529 ms (86.18%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -6,10 +6,3 @@ on s.student_id=e.student_id and sub.subject_name=e.subject_name
 group by sub.subject_name,s.student_id
 order by s.student_id,sub.subject_name;
 
--- select b.student_id, b.student_name, a.subject_name,count(c.subject_name) as attended_exams
--- from subjects as a
--- cross join students as b
--- left join examinations as c
--- on b.student_id = c.student_id and a.subject_name = c.subject_name
--- group by b.student_id,a.subject_name
--- order by b.student_id;
