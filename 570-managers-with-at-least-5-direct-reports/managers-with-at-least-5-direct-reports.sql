68e1b4c79023b077f761a89a47ac6f9937872cf3
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-03-23 15:52:29 +0530

    Time: 668 ms (47.62%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -3,4 +3,5 @@
 
 select a.name from Employee as a 
 join Employee as b 
-on a.id=b.managerId group by a.id having COUNT(b.managerId) >= 5;
+on a.id=b.managerId
+group by a.id having COUNT(b.managerId) >= 5;
