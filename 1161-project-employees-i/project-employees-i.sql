795f3453308a4844444dfc5ab96108f7a9b35730
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-03-29 19:16:00 +0530

    Time: 1371 ms (17.06%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,7 +1,9 @@
 select p.project_id, 
-round(sum(e.experience_years)/count(case when e.experience_years is not null then 1 end),2) 
+round(sum(e.experience_years)/count(e.experience_years),2) 
 as average_years
 from Project as p 
 left join Employee as e 
 on p.employee_id=e.employee_id 
 group by p.project_id;
+
+-- (case when e.experience_years is not null then 1 end)
