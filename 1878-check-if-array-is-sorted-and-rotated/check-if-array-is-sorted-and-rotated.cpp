edd2d81fd7ae4664ccda57ebb7d9b497e842b218
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-04-11 00:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (17.41%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -3,11 +3,15 @@ public:
     bool check(vector<int>& nums) {
         int count = 0;
         int n=nums.size();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (nums[i] > nums[(i + 1) % n]) {
                 ++count;
             }
         }
         return count <= 1;
+        // This line returns true if the count of inversions (cnt) is 
+        // less than or equal to 1, indicating that the vector is 
+        // almost sorted (at most one inversion is allowed). Otherwise, 
+        // it returns false.
     }
 };
