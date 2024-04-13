397df6de216fde14ea79842002dad64516c8e8fd
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-04-13 16:37:23 +0530

    Time: 22 ms (81.15%) | Memory: 38.6 MB (36.10%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -5,7 +5,7 @@ public:
         int cnt=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==1){
-                cnt++;
+                cnt++; 
                 maxi=max(maxi,cnt);
             }
             else{
