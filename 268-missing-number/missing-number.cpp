b566d60e325cfd8f8ce8aee7bb0fcc7de72cc6c8
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-04-14 01:45:04 +0530

    Time: 15 ms (50.41%) | Memory: 20.3 MB (60.07%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,37 +1,37 @@
 // APPROACH 1: BETTER
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        vector <int> hash(n+1,0);
-        for(int i=0;i<n;i++){
-            hash[nums[i]]++;
-        }
-        for(int i=0;i<hash.size();i++){
-            if(hash[i]==0){
-                return i;
-            }
-        }
-        return 0;
-    }
-};
-
-// APPROACH 2:OPTIMAL
 // class Solution {
 // public:
 //     int missingNumber(vector<int>& nums) {
 //         int n=nums.size();
-//         int sum=0;
-//         int sum2=0;
-//         for(int i=1;i<=n;i++){
-//             sum+=i;
-//         }
+//         vector <int> hash(n+1,0);
 //         for(int i=0;i<n;i++){
-//             sum2+=nums[i];
+//             hash[nums[i]]++;
 //         }
-//         return sum-sum2;
+//         for(int i=0;i<hash.size();i++){
+//             if(hash[i]==0){
+//                 return i;
+//             }
+//         }
+//         return 0;
 //     }
 // };
+
+// APPROACH 2:OPTIMAL
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        int sum2=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        for(int i=0;i<n;i++){
+            sum2+=nums[i];
+        }
+        return sum-sum2;
+    }
+};
 
 // APPROACH 3:MORE OPTIMAL
 // class Solution {
