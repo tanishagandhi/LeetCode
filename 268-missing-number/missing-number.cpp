e5cc4f92e3f6970810cc3dde7b23bdac4114524c
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-04-14 16:35:05 +0530

    Time: 7 ms (96.23%) | Memory: 20.4 MB (60.07%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -17,25 +17,33 @@
 // };
 
 // APPROACH 2:OPTIMAL
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n=nums.size();
-        int sum=0;
-        int sum2=0;
-        for(int i=1;i<=n;i++){
-            sum+=i;
-        }
-        for(int i=0;i<n;i++){
-            sum2+=nums[i];
-        }
-        return sum-sum2;
-    }
-};
-
-// APPROACH 3:MORE OPTIMAL
 // class Solution {
 // public:
 //     int missingNumber(vector<int>& nums) {
+//         int n=nums.size();
+//         int sum=0;
+//         int sum2=0;
+//         for(int i=1;i<=n;i++){
+//             sum+=i;
+//         }
+//         for(int i=0;i<n;i++){
+//             sum2+=nums[i];
+//         }
+//         return sum-sum2;
 //     }
 // };
+
+// APPROACH 3:MORE OPTIMAL
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1=0;
+        int xor2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            xor2=xor2^nums[i];
+            xor1=xor1^(i+1);
+        }
+        return xor1^xor2;
+    }
+};
