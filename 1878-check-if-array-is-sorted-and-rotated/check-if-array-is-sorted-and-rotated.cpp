3660720262726666235e7449d16245741a45e883
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-04-14 15:09:43 +0530

    Time: 2 ms (45.03%) | Memory: 10.2 MB (56.44%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -4,7 +4,7 @@ public:
         int cnt=0;
         int n=nums.size();
         for(int i=0;i<n;i++){
-            if (nums[i]>nums[(i+1)%n])  cnt++;
+            if (nums[i]>nums[(i+1)%n])  ++cnt;
         }
         return cnt<=1;
         // This line returns true if the count of inversions (cnt) is 
