3c31f9a4486d1a42de2d7d3915531ca3503c9f16
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2024-08-08 13:24:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (34.18%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -10,17 +10,43 @@
  * };
  */
 
+//RECURSIVE SOLUTION:-
+// class Solution {
+// public:
+//     void inorder(TreeNode* root,vector<int> &ans){
+//         if(root==NULL) return;
+//         inorder(root->left,ans);
+//         ans.push_back(root->val);
+//         inorder(root->right,ans);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> ans;
+//         inorder(root,ans);
+//         return ans;
+//     }
+// };
+
+//ITERATIVE SOLUTION:-
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int> &ans){
-        if(root==NULL) return;
-        inorder(root->left,ans);
-        ans.push_back(root->val);
-        inorder(root->right,ans);
-    }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> ans;
-        inorder(root,ans);
+        if(root==NULL) return ans;
+        stack <TreeNode*> st;
+        TreeNode* node=root;
+        while(true){
+            if(node!=NULL){
+                st.push(node);
+                node= node->left;
+            }
+            else{
+                if(st.empty()==true) break;
+                node=st.top();
+                st.pop();
+                ans.push_back(node->val);
+                node = node->right;
+            }
+        }
         return ans;
     }
 };
