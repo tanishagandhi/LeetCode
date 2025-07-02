7d9f2517166bc10cedeed4dda09e2c7227bd5c55
Tanisha Gandhi <152962388+tanishagandhi@users.noreply.github.com>
2025-07-02 12:35:36 +0530

    Time: 3 ms (31.55%) | Memory: 10.7 MB (6.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -7,11 +7,15 @@ public:
                 ans += tolower(c);
             }
         }
-        for(int i=0; i<ans.size()/2; i++){
-            if(ans[i]!=ans[ans.size()-i-1]){
-                return false;
-            }
-        }
-        return true;
+        // for(int i=0; i<ans.size()/2; i++){
+        //     if(ans[i]!=ans[ans.size()-i-1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        string ans2=ans;
+        reverse(ans.begin(),ans.end());
+        if(ans==ans2) return true;
+        else return false;
     }
 };
